4c173df00c1ce96ff6aed936a34eef21c4e6aaa3
Brian Zhao <zhaoyubrian@gmail.com>
2020-05-26 22:53:27 +1000

    Python GIL
    - concurrency, using threading.
    - parallelism, using sub-processes.

---
+++ PyGIL/parallelism.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import time
+
+nums = [1 for _ in range(100000000)]
+chunk_size = len(nums) // 10
+readers = []
+
+while nums:
+    chunk, nums = nums[:chunk_size], nums[chunk_size:]
+    reader, writer = os.pipe()
+    if os.fork():
+        readers.append(reader)  # Parent.
+    else:
+        # Child process.
+        subtotal = 0
+
+        # Intentionally slow code.
+        for i in chunk:
+            subtotal += i
+
+        print('subtotal %d' % subtotal)
+
+        # Send result to parent, and quit.
+        os.write(writer, str(subtotal).encode())
+        sys.exit(0)
+
+start = time.time()
+# Parent.
+total = 0
+for reader in readers:
+    subtotal = int(os.read(reader, 1000).decode())
+    total += subtotal
+
+print("Total: %d" % total)
+print('%.2f seconds' % (time.time() - start))
