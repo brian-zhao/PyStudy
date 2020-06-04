0c5f1cbd419157bb1ef0789ef7bc81eea6df588b
Brian Zhao <zhaob@google.com>
2020-06-04 22:00:47 +1000

    WIP desgin pattern

---
+++ patterns/singleton/main.py
@@ -0,0 +1,35 @@
+
+class SingletonMeta(type):
+  """
+  The Singleton class can be implemented in different ways in Python. Some
+  possible methods include: base class, decorator, metaclass. We will use the
+  metaclass because it is best suited for this purpose.
+  """
+
+  _instances = {}
+
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+    return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+  def some_business_logic(self):
+    """
+    Finally, any singleton should define some business logic, which can be
+    executed on its instance.
+    """
+    pass
+    # print('Singleton Pattern')
+
+
+if __name__ == "__main__":
+  # The client code
+  s1 = Singleton()
+  s2 = Singleton()
+
+  if id(s1) == id(s2):
+    print("Singleton works, both variables contain the same instance.")
+  else:
+    print("Singleton failed, variables contain different instances.")
